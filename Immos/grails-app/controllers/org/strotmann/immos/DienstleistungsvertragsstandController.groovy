1a64fa53581803cad919104bf14e057ac88736ef
Peter Strotmann <peter@strotmann.org>
2019-04-13 16:10:49 +0200

    Absturz Liste Dienstleistungsvertragsstand beseitigt

--- Immos/grails-app/controllers/org/strotmann/immos/DienstleistungsvertragsstandController.groovy
+++ Immos/grails-app/controllers/org/strotmann/immos/DienstleistungsvertragsstandController.groovy
@@ -5,4 +5,9 @@ import org.springframework.dao.DataIntegrityViolationException
 class DienstleistungsvertragsstandController {
 
     def scaffold = true
+	
+	def list(Integer max) {
+		params.max = Math.min(max ?: 100, 1000)
+		[dienstleistungsvertragsstandInstanceList: Dienstleistungsvertragsstand.list(), dienstleistungsvertragsstandInstanceTotal: Dienstleistungsvertragsstand.count()]
+	}
 }
