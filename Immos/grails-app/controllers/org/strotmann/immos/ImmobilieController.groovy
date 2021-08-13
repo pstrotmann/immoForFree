076d784e3e45b4ff1783bc1ffa25051494e4d676
Peter Strotmann <peter@strotmann.org>
2021-08-13 12:32:01 +0200

    Korrektur Immo Update + Webservice ImmoSum

--- Immos/grails-app/controllers/org/strotmann/immos/ImmobilieController.groovy
+++ Immos/grails-app/controllers/org/strotmann/immos/ImmobilieController.groovy
@@ -21,4 +21,40 @@ class ImmobilieController {
 		flash.teilsummen = immobilieInstance.teilsummen
 		[immobilieInstance: immobilieInstance]
 	}
+	
+	def update(Long id, Long version) {
+		def immobilieInstance = Immobilie.get(id)
+		if (!immobilieInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'immobilie.label', default: 'Immobilie'), id])
+			redirect(action: "list")
+			return
+		}
+
+		if (version != null) {
+			if (immobilieInstance.version > version) {
+				immobilieInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						  [message(code: 'immobilie.label', default: 'Immobilie')] as Object[],
+						  "Another user has updated this Immobilie while you were editing")
+				render(view: "edit", model: [immobilieInstance: immobilieInstance])
+				return
+			}
+		}
+
+		immobilieInstance.properties = params
+
+		if (!immobilieInstance.save(flush: true)) {
+			render(view: "edit", model: [immobilieInstance: immobilieInstance])
+			return
+		}
+
+		flash.message = message(code: 'default.updated.message', args: [message(code: 'immobilie.label', default: 'Immobilie'), immobilieInstance.id])
+		redirect(action: "show", id: immobilieInstance.id)
+	}
+	
+	def immoSum() {render sum()}
+	
+	String sum () {
+		Immobilie.immobilienUndSummen.sumVerkauf.toString().replace('.', ',')
+	}
+	
 }
